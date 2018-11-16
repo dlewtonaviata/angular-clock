802130f930c42309d5225de93bc008bee4a95439
dlewton <dlewton@aviatainc.com>
2018-11-16 09:58:23 -0700

    Add digital clock component with simplistic styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DigitalClockComponent } from './digital-clock/digital-clock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DigitalClockComponent
   ],
   imports: [
     BrowserModule,
