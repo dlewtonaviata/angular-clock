dacb541108f436dc3bd719f42d1b6c8cd058502d
dlewton <dlewton@aviatainc.com>
2018-11-16 16:32:26 -0700

    Request current time from NIST server, then update locally

--- src/app/digital-clock/digital-clock.component.ts
+++ src/app/digital-clock/digital-clock.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 
 import { TimeTrackingService } from '../time-tracking.service';
 
@@ -7,14 +7,21 @@ import { TimeTrackingService } from '../time-tracking.service';
   templateUrl: './digital-clock.component.html',
   styleUrls: ['./digital-clock.component.scss']
 })
-export class DigitalClockComponent implements OnInit {
-  time: string;
+export class DigitalClockComponent implements OnInit, OnDestroy {
+  subscriptions = [];
+  time;
 
   // Constructor shouldn't DO anything... Especially use a service!
   constructor(private timeTrackingService: TimeTrackingService) { }
 
   // Use ngOnInit for any possibly complex initialization
   ngOnInit() {
-    this.timeTrackingService.getTime().subscribe(time => this.time = time);
+    this.subscriptions.push(
+      this.timeTrackingService.getTime().subscribe(time => this.time = time)
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 }
--- src/app/time-tracking.service.ts
+++ src/app/time-tracking.service.ts
@@ -1,14 +1,36 @@
 import { Injectable } from '@angular/core';
-import { of, Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { interval, Observable, concat } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TimeTrackingService {
+  static timeServer = 'https://nist.time.gov/actualtime.cgi?disablecache=1542404055741&__lzbc__=zcr7up';
+  static headers: HttpHeaders = new HttpHeaders().set('Content-Type', 'text/xml');
 
-  constructor() { }
+  time: number;
 
-  getTime (): Observable<string> {
-    return of('1542394421635902');
+  constructor(private httpClient: HttpClient) { }
+
+  resetTime (): Observable<number> {
+    return this.httpClient
+      .get(TimeTrackingService.timeServer, { responseType: 'text' })
+      .pipe(map(value => {
+        const timeStr = value.split(' ')
+          .filter(item => /^time=/.test(item.substr(0, 5)))
+          .map(item => item.split('').filter(x => /[0-9]/.test(x)).join(''))
+          .pop();
+
+        return (this.time = (+timeStr / 1000));
+      }));
+  }
+
+  getTime (): Observable<number> {
+    const initial = this.resetTime();
+    const tracker = interval(1000).pipe(map(_ => this.time = this.time + 1000));
+
+    return concat(initial, tracker);
   }
 }
